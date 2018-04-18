65122bca85c7d1ae6d6e149fcf1663713be7373a
IgrisUVG <igris@ushivangoga.ru>
2018-04-18 23:47:31 +0300

    Img-

--- scripts/addBuch.php
+++ scripts/addBuch.php
@@ -1,8 +1,8 @@
 <?php
 require '../com/connect.php';
 
-$upload_dir = SITE_ROOT . 'covers';
-$image_fieldname = 'cover_pic';
+//$upload_dir = SITE_ROOT . 'covers';
+//$image_fieldname = 'cover_pic';
 
 $first_name = trim($_REQUEST['first_name']);
 $second_name = trim($_REQUEST['second_name']);
@@ -10,11 +10,11 @@ $last_name = trim($_REQUEST['last_name']);
 $title = trim($_REQUEST['title']);
 $inhalt = trim($_REQUEST['inhalt']);
 $description = trim($_REQUEST['description']);
-$schrank_num = trim($_REQUEST['shcrank_num']);
+$schrank_num = trim($_REQUEST['schrank_num']);
 $regal_num = trim($_REQUEST['regal_num']);
 
-@move_uploaded_file($_FILES[$image_fieldname]['tmp_name'], $upload_filename)
-    or die("возникла проблема сохранения вашего изображения в его постоянном месте. Ошибка, связанная с правами доступа при перемещении файла в {$upload_filename}");
+//@move_uploaded_file($_FILES[$image_fieldname]['tmp_name'], $upload_filename)
+//    or die("возникла проблема сохранения вашего изображения в его постоянном месте. Ошибка, связанная с правами доступа при перемещении файла в {$upload_filename}");
 
 $insert_sql = "
   INSERT INTO autors (
@@ -25,8 +25,8 @@ $insert_sql = "
       inhalt, 
       description, 
       schrank_num, 
-      regal_num, 
-      cover) 
+      regal_num
+      ) 
   VALUES (
       '{$first_name}', 
       '{$second_name}', 
@@ -35,8 +35,7 @@ $insert_sql = "
       '{$inhalt}', 
       '{$description}', 
       '{$schrank_num}', 
-      '{$regal_num}', 
-      '{$image_fieldname}'
+      '{$regal_num}'
       );
 ";
 mysqli_query($mysqli, $insert_sql) or die(mysqli_error($mysqli));
