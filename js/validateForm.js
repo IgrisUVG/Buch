21b555e446d943a5456dd4d12318414c8776fac9
IgrisUVG <igris@ushivangoga.ru>
2018-04-24 18:49:52 +0300

    Validate++

--- js/validateForm.js
+++ js/validateForm.js
@@ -1,22 +1,17 @@
 function validateForm() {
     var x = document.forms['form']['first_name'].value;
     if (x == null || x == '' || x.length==0) {
-        document.getElementById('namef').innerHTML='*обязательно для заполнения';
+        document.getElementById('first_name').value='Заполнить';
         return false;
     }
     var y = document.forms['form']['last_name'].value;
     if (y == null || y == '' || y.length==0) {
-        document.getElementById('namel').innerHTML='*обязательно для заполнения';
+        document.getElementById('last_name').value='Заполнить';
         return false;
     }
     var z = document.forms['form']['title'].value;
     if (z == null || z == '' || z.length==0) {
-        document.getElementById('titel').innerHTML='*обязательно для заполнения';
+        document.getElementById('title').value='Заполнить';
         return false;
     }
 }
-document.getElementById('reset').onclick = function () {
-    document.getElementById('namef').innerHTML = '';
-    document.getElementById('namel').innerHTML = '';
-    document.getElementById('titel').innerHTML = '';
-};
