14f0531007bff85bcaec8dcb350a056e999a847a
IgrisUVG <igris@ushivangoga.ru>
2018-04-25 18:36:44 +0300

    Form+

--- js/validateForm.js
+++ js/validateForm.js
@@ -6,18 +6,20 @@ function validateForm() {
     // }
     var y = document.forms['form']['last_name'].value;
     if (y == null || y == '' || y.length == 0) {
-        document.getElementById('last_name').value = 'Заполните это поле';
-        document.getElementById('last_name').style.backgroundColor = 'red';
+        document.getElementById('last_name').placeholder = 'Заполните это поле';
+        // document.getElementById('last_name').style.color = 'red';
+        // document.getElementById('submit').style.pointerEvents = none;
         return false;
     }
     var z = document.forms['form']['title'].value;
     if (z == null || z == '' || z.length == 0) {
-        document.getElementById('title').value = 'Заполните это поле';
-        document.getElementById('title').style.backgroundColor = 'red';
+        document.getElementById('title').placeholder = 'Заполните это поле';
+        // document.getElementById('title').style.color = 'red';
+        // document.getElementById('submit').style.pointerEvents = none;
         return false;
     }
 }
 document.getElementById('reset').onclick = function () {
-    document.getElementById('last_name').style.backgroundColor = '';
-    document.getElementById('title').style.backgroundColor = '';
+    document.getElementById('last_name').placeholder = '';
+    document.getElementById('title').placeholder = '';
 };
