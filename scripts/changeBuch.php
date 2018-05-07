3062ac666ca7216d5197720f3f1e6db264ea02c1
IgrisUVG <igris@ushivangoga.ru>
2018-05-07 17:12:16 +0300

    Change+

--- scripts/changeBuch.php
+++ scripts/changeBuch.php
@@ -1,68 +1,3 @@
-<?php
-require '../com/connect.php';
-$autor_id = $_REQUEST['autor_id'];
-$select_query = "SELECT * FROM autors WHERE autor_id=" . $autor_id;
-$result = mysqli_query($mysqli, $select_query);
-if ($result) {
-    $row = $result->fetch_array();
-    $autor_id = $row['autor_id'];
-    $autor = $row['autor'];
-    $first_name = $row['first_name'];
-    $second_name = $row['second_name'];
-    $last_name = $row['last_name'];
-    $title = $row['title'];
-    $inhalt = $row['inhalt'];
-    $description = preg_replace("/[\r\n]+/", "</p><p>", $row['description']);
-    $schrank_num = $row['schrank_num'];
-    $regal_num = $row['regal_num'];
-    $druck = $row['druck'];
-    $notes = $row['notes'];
-//    $image_query = "SELECT * FROM images WHERE image_id=" . $cover;
-//    $image_result = mysqli_query($mysqli, $image_query);
-} else {
-    handle_error("возникла проблема с поиском вашей информации на нашей системе.", "Ошибка обнаружения пользователя с ID {$autor_id}");
-}
-if (
-    isset($_POST['autor_id']) &&
-    isset($_POST['autor']) &&
-    isset($_POST['title']) &&
-    isset($_POST['inhalt']) &&
-    isset($_POST['druck']) &&
-    isset($_POST['description']) &&
-    isset($_POST['notes']) &&
-    isset($_POST['schrank_num']) &&
-    isset($_POST['regal_num'])
-//        $autor_id=$_POST['autor_id'] &&
-//        $autor=$_POST['autor'] &&
-//        $title=$_POST['title'] &&
-//        $inhalt=$_POST['inhalt'] &&
-//        $druck=$_POST['druck'] &&
-//        $description=$_POST['description'] &&
-//        $notes=$_POST['notes'] &&
-//        $schrank_num=$_POST['schrank_num'] &&
-//        $regal_num=$_POST['regal_num']
-) {
-//    $autor = htmlentities(mysqli_real_escape_string($mysqli, $_POST['autor']));
-    $query = "UPDATE autors SET
-              autor_id=$autor_id,
-              autor=$autor,
-              title=$title,
-              inhalt=$inhalt,
-              druck=$druck,
-              description=$description,
-              notes=$notes,
-              schrank_num=$schrank_num,
-              regal_num=$regal_num
-              WHERE autor_id=$autor_id
-             ";
-
-    $res = mysqli_query($mysqli, $query) or die("Ошибка " . mysqli_error($mysqli));
-
-    if ($res) {
-        header("Location: showBuch.php?autor_id=" . $autor_id);
-    }
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -75,42 +10,103 @@ if (
 <div id="headerMain">
     <div id="header"><h1>Библиотека</h1></div>
 </div>
-<div id="content">
-    <form name="form" onsubmit="return validateForm()" action="" method="POST" enctype="multipart/form-data">
-        <fieldset>
-            <label for="autor">Автор:</label>
-            <input type="text" id="autor" name="autor" size="41" value="<?php echo $autor ?>"/><br/>
-            <label for="title">Название:</label>
-            <input type="text" id="title" name="title" size="41" value="<?php echo $title ?>"/><br/>
-            <label for="inhalt">Содержание:</label>
-            <textarea id="inhalt" name="inhalt" cols="40" rows="8"><?php echo $inhalt ?></textarea><br/>
-            <label for="druck">Издательство:</label>
-            <input type="text" id="druck" name="druck" size="41" value="<?php echo $druck ?>"/><br/>
-            <label for="description">Описание:</label>
-            <textarea id="description" name="description" cols="40" rows="8"><?php echo $description ?></textarea><br/>
-            <label for="notes">Для заметок:</label>
-            <input type="text" id="notes" name="notes" size="41" value="<?php echo $notes ?>"/><br/>
-            <!--Добавление картинки-->
-            <!--            <input type="hidden" name="MAX_FILE_SIZE" value="2000000"/>-->
-            <!--            <label for="cover">Обложка:</label>-->
-            <!--            <!--<div class="file_upload">-->
-            <!--                <!--<button class="but" type="button">Выберите файл</button>-->
-            <!--                <!--<div>Файл не выбран</div>-->
-            <!--                <!--<input type="file" id="cover" name="cover" size="30"/>-->
-            <!--            <!--</div>-->
-            <!--            <input type="file" id="cover" name="cover" size="30"/><br/>-->
-            <!--|-->
-            <label for="schrank_num">Шкаф:</label>
-            <input type="text" id="schrank_num" name="schrank_num" size="10" value="<?php echo $schrank_num ?>"/><br/>
-            <label for="regal_num">Полка:</label>
-            <input type="text" id="regal_num" name="regal_num" size="10" value="<?php echo $regal_num ?>"/><br/>
-        </fieldset>
-        <br/>
-        <fieldset class="center">
-            <input class="but" id="submit" type="submit" value="Изменить"/>
-        </fieldset>
-    </form>
-</div>
+<?php
+require '../com/connect.php';
+if (
+    isset($_POST['autor_id']) &&
+    isset($_POST['autor']) &&
+    isset($_POST['title']) &&
+    isset($_POST['inhalt']) &&
+    isset($_POST['druck']) &&
+    isset($_POST['description']) &&
+    isset($_POST['notes']) &&
+    isset($_POST['schrank_num']) &&
+    isset($_POST['regal_num'])
+) {
+    $autor_id = htmlentities(mysqli_real_escape_string($mysqli, $_POST['autor_id']));
+    $autor = htmlentities(mysqli_real_escape_string($mysqli, $_POST['autor']));
+    $title = htmlentities(mysqli_real_escape_string($mysqli, $_POST['title']));
+    $inhalt = htmlentities(mysqli_real_escape_string($mysqli, $_POST['inhalt']));
+    $druck = htmlentities(mysqli_real_escape_string($mysqli, $_POST['druck']));
+    $description = htmlentities(mysqli_real_escape_string($mysqli, $_POST['description']));
+    $notes = htmlentities(mysqli_real_escape_string($mysqli, $_POST['notes']));
+    $schrank_num = htmlentities(mysqli_real_escape_string($mysqli, $_POST['schrank_num']));
+    $regal_num = htmlentities(mysqli_real_escape_string($mysqli, $_POST['regal_num']));
+
+    $query = "UPDATE autors SET
+              autor='$autor',
+              title='$title',
+              inhalt='$inhalt',
+              druck='$druck',
+              description='$description',
+              notes='$notes',
+              schrank_num='$schrank_num',
+              regal_num='$regal_num'
+              WHERE autor_id='$autor_id'
+             ";
+
+    $result = mysqli_query($mysqli, $query) or die("Ошибка " . mysqli_error($mysqli));
+
+    if ($result) {
+        header("Location: showBuch.php?autor_id=" . $autor_id);
+    }
+}
+
+if (isset($_GET['autor_id'])) {
+    $autor_id = htmlentities(mysqli_real_escape_string($mysqli, $_GET['autor_id']));
+
+    $query = "SELECT * FROM autors WHERE autor_id='$autor_id'";
+
+    $result = mysqli_query($mysqli, $query) or  die("Ошибка " . mysqli_error($mysqli));
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_row($result);
+        $autor = $row[1];
+        $first_name = $row[2];
+        $second_name = $row[3];
+        $last_name = $row[4];
+        $title = $row[5];
+        $inhalt = $row[6];
+        $description = preg_replace("/[\r\n]+/", "</p><p>", $row[7]);
+        $schrank_num = $row[9];
+        $regal_num = $row[10];
+        $druck = $row[11];
+        $notes = $row[12];
+
+        echo "
+            <div id='content'>
+                <form name='form' onsubmit='return validateForm()' method='POST'>
+                    <fieldset>
+                        <label for='autor'>Автор:</label>
+                        <input type='text' id='autor' name='autor' size='41' value='$autor'/><br/>
+                        <label for='title'>Название:</label>
+                        <input type='text' id='title' name='title' size='41' value='$title'/><br/>
+                        <label for='inhalt'>Содержание:</label>
+                        <textarea id='inhalt' name='inhalt' cols='40' rows='8'>$inhalt</textarea><br/>
+                        <label for='druck'>Издательство:</label>
+                        <input type='text' id='druck' name='druck' size='41' value='$druck'/><br/>
+                        <label for='description'>Описание:</label>
+                        <textarea id='description' name='description' cols='40' rows='8'>$description</textarea><br/>
+                        <label for='notes'>Для заметок:</label>
+                        <input type='text' id='notes' name='notes' size='41' value='$notes'/><br/>
+                        <label for='schrank_num'>Шкаф:</label>
+                        <input type='text' id='schrank_num' name='schrank_num' size='10' value='$schrank_num'/><br/>
+                        <label for='regal_num'>Полка:</label>
+                        <input type='text' id='regal_num' name='regal_num' size='10' value='$regal_num'/><br/>
+                    </fieldset>
+                    <br/>
+                    <fieldset class='center'>
+                        <input class='but' id='submit' type='submit' value='Сохранить'/>
+                    </fieldset>
+                </form>
+            </div>
+            ";
+
+        mysqli_free_result($result);
+    }
+}
+mysqli_close($mysqli);
+?>
 <div id="footer">
     <div class="buttons">
         <a class="but" href="../index.php">В начало</a>
