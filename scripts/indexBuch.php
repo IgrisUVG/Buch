6ed6acdc8b50e2183a0130473864589736b4d91e
IgrisUVG <igris@ushivangoga.ru>
2018-05-11 17:11:59 +0300

    Sort

--- scripts/indexBuch.php
+++ scripts/indexBuch.php
@@ -1,6 +1,17 @@
 <?php
 require '../com/connect.php';
-$result = $mysqli->query('SELECT * FROM autors ORDER BY autor');
+$sql = 'SELECT * FROM autors';
+if ($_GET['sort'] == 'autor_desc') {
+    $sql .= ' ORDER BY autor DESC';
+} elseif ($_GET['sort'] == 'title') {
+    $sql .= ' ORDER BY title';
+} elseif ($_GET['sort'] == 'title_desc') {
+    $sql .= ' ORDER BY title DESC';
+} else {
+    $sql .= ' ORDER BY autor';
+}
+//$result = $mysqli->query('SELECT * FROM autors ORDER BY autor');
+$result = $mysqli->query($sql);
 while ($row = $result->fetch_assoc()) {
     $autor_id = $row['autor_id'];
     $autor = $row['autor'];
