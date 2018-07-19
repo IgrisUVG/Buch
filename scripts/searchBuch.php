798cb5e203d4c3f2b1a64b8dcfd25227f45a2c97
IgrisUVG <igris@ushivangoga.ru>
2018-07-19 17:55:15 +0300

    Add search+?

--- scripts/searchBuch.php
+++ scripts/searchBuch.php
@@ -28,7 +28,20 @@ if (!empty($_POST["schrank_num"])) {
     if (!empty($_POST["regal_num"])) {
 
         $regal_num = trim(strip_tags(stripcslashes(htmlspecialchars($_POST["regal_num"]))));
-        $temp_schrank = "CREATE TEMPORARY TABLE temp_schrank SELECT * FROM autors WHERE schrank_num LIKE '%$schrank_num%'";
+//        $temp_schrank = "CREATE TEMPORARY TABLE temp_schrank SELECT * FROM autors WHERE schrank_num LIKE '%$schrank_num%'";
+
+//        if (mysqli_query($mysqli, $temp_schrank)) {
+        $db_regal_num = $mysqli->query("
+                                         CREATE TEMPORARY TABLE temp_schrank
+                                         SELECT * FROM autors WHERE schrank_num LIKE '%$schrank_num%';
+                                         SELECT * FROM temp_schrank WHERE regal_num LIKE '%$regal_num%'
+                                        ")
+        or die('Ошибка №' . __LINE__ . '<br>Обратитесь к администратору сайта пожалуйста, сообщив номер ошибки.');
+
+        while ($row = $db_regal_num->fetch_array()) {
+            echo "\n<li><table><tr><td><a href='../scripts/showBuch.php?autor_id=" . $row["autor_id"] . "' title='" . $row["title"] . "'>" . $row["autor"] . "</a></td><td><a href='../scripts/showBuch.php?autor_id=" . $row["autor_id"] . "' title='" . $row["autor"] . "'>" . $row["title"] . "</a></td><td>Шкаф " . $row["schrank_num"] . ", полка " . $row["regal_num"] . "</td></tr></table></li>";
+        }
     }
+//    }
 
 }
